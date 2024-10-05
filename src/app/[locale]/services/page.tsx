44a3e186fb8ede9d522c9a51486edaff5ca885b6
Mashal Khan <usamakhanxxx00@gmail.com>
2024-10-05 01:08:02 -0700

    SEO-Service-Com

--- src/app/[locale]/services/page.tsx
+++ src/app/[locale]/services/page.tsx
@@ -3,14 +3,51 @@ import ContactUs from "@/components/ContactForm";
 import Footer from "@/components/Home/Footer";
 import OurServices from "@/components/Home/OurServices";
 import PickUpRentalServices from "@/components/Home/PickUpRentalServices";
-
 import Navbar from "@/components/Navbar";
 import HealthcareSlider from "@/components/OtherMovingServices";
-
 import ServiceTopSection from "@/components/ServiceTopSection";
 import TestimonialSection from "@/components/Testimonials";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 import React from "react";
 
+interface Params {
+  params: {
+    locale: string;
+  };
+}
+export async function generateMetadata({ params }: Params): Promise<Metadata> {
+  const locale = params.locale;
+  const t = await getTranslations({ locale, namespace: "meta-service" });
+
+  return {
+    title: t("title"),
+    description: t("desc"),
+    alternates: {
+      canonical: process.env.PUBLIC_URL,
+      languages: {
+        ar: `${process.env.PUBLIC_URL}/ar`,
+        en: `${process.env.PUBLIC_URL}/en`,
+      },
+    },
+    openGraph: {
+      title: t("title"),
+      description: t("desc"),
+      url: process.env.PUBLIC_URL,
+      siteName: t("site-Info.name"),
+      locale: locale,
+      type: "website",
+      alternateLocale: ["ar", "en"],
+      countryName: t("site-Info.country"),
+      faxNumbers: ["+971566651978", "+971545019655", "+971503626685"],
+      phoneNumbers: ["+971566651978", "+971545019655", "+971503626685"],
+      images: {
+        url: `${process.env.PUBLIC_URL}/images/hamdan-mover-logo.png`,
+        alt: "Hamdan Movers Logo",
+      },
+    },
+  };
+}
 function page() {
   return (
     <>
