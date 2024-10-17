1a3895e1b3e9074246cb2a2beac48505a1886c84
Mashal Khan <usamakhanxxx00@gmail.com>
2024-10-17 06:30:01 -0700

    update-files

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,25 +27,25 @@ export default async function customMiddleware(req: NextRequest) {
     return createMiddleware(routing)(req);
   }
 
-  //logic to checking requests that is coming from arabic country
-  // async function getLocale(req: NextRequest): Promise<string> {
-  //   const ip = req.ip || req.headers.get("x-forwarded-for");
+  // logic to checking requests that is coming from arabic country
+  async function getLocale(req: NextRequest): Promise<string> {
+    const ip = req.ip || req.headers.get("x-forwarded-for");
 
-  //   const geoResponse = await fetch(
-  //     `https://ipinfo.io/${ip}?token=979f9b266e141e`
-  //   );
-  //   const data = await geoResponse.json();
-  //   console.log({ ...data });
+    const geoResponse = await fetch(
+      `https://ipinfo.io/${ip}?token=979f9b266e141e`
+    );
+    const data = await geoResponse.json();
+    console.log({ ...data });
 
-  //   return data.country;
-  // }
+    return data.country;
+  }
 
-  // const isArabic = await getLocale(req);
+  const isArabic = await getLocale(req);
 
   // checking is araibc country
-  // if (ARABIC_COUNTRIES.includes(isArabic)) {
-  //   return NextResponse.redirect(new URL(`/ar${pathname}`, req.url));
-  // }
+  if (ARABIC_COUNTRIES.includes(isArabic)) {
+    return NextResponse.redirect(new URL(`/ar${pathname}`, req.url));
+  }
 
   //in case of non arabic country
   return NextResponse.redirect(new URL(`/en${pathname}`, req.url));
